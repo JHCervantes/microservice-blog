c9dd7756c438f61510983dd05f683a02c21301b7
Jesus Cervantes <impurepd@gmail.com>
2020-04-28 14:55:05 -0600

    first commit

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default ({ postId }) => {
+  const [content, setContent] = useState("");
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
+      content
+    });
+
+    setContent("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="">New Comment</label>
+          <input
+            value={content}
+            onChange={e => setContent(e.target.value)}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
